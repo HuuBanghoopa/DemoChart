fca509d72296b227e174c8de50097ec9f0ddcf34
HuuBanghoopa <huubagn@gmail.com>
2025-04-20 09:45:46 +0700

    Delete DemoChart directory

--- DemoChart/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoChart.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
